102ad2fbfb4c538402a1eea0bb7e6df5a626dae1
Francisco Junior <jr-francis2011@hotmail.com>
2020-04-02 16:01:51 -0300

    Binary Operations

---
+++ BinaryOperations.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB09
+{
+    public partial class FRMlab09 : Form
+    {
+        // área inicial do formulário para declarações globais/públicas.
+        float numero1, numero2;
+        double result;
+        // acima: variáveis que serão usadas por todos os clicks dos botões.
+
+        public FRMlab09()
+        {
+            InitializeComponent();
+        }
+
+        private bool valida_entrada() // função não associada a objeto/evento,
+        {                             // que retorna um valor lógico (boolean).
+            try
+            {
+                numero1 = float.Parse(TXTnumero1.Text);
+                numero2 = float.Parse(TXTnumero2.Text);
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("ERRO!  Campos vazios ou não numéricos.  Redigite.");
+                BTNlimpar.PerformClick();       
+                return false;
+            }
+        }
+
+        private void saida(Color cor) // função não associada a objeto/evento.
+        {
+            // Recebe parâmetro  COR  que é usado no segundo comando abaixo.
+            // Este parâmetro é do tipo  Color e seu valor é dado por  Color.nome_da_cor 
+            LBLsaida.Text = result.ToString();
+            LBLsaida.BackColor = cor;
+            TXTnumero1.Focus();
+        }
+
+        private void FRMlab09_Load(object sender, EventArgs e)
+        {
+            // para procedimentos iniciais do formulário.
+            BTNlimpar.PerformClick();
+        }
+
+        private void TXTnumero1_TextChanged(object sender, EventArgs e)
+        {
+            // Programar o evento "TextChanged" é uma alternativa interessante.
+            // A função valida_entrada() pode ser chamada daqui ao invés de sê-lo
+            // no evento Click dos botões de cálculo. Neste caso é necessário
+            // adaptar o controle de retorno (true ou false) e tratar cada campo
+            // TXTvalor1 e TXTvalor2 separadamente, com mensagens de erro personalidadas.
+        }
+
+        private void TXTnumero2_TextChanged(object sender, EventArgs e)
+        {
+            // Aqui valem as mesmas observações da função acima.
+        }
+
+        private void BTNsoma_Click(object sender, EventArgs e)
+        {
+            if (valida_entrada())
+            {
+                // cálculo específico deste "objeto_evento"
+                result = (double)(numero1 + numero2);
+                saida(Color.Silver);
+            }
+        }
+
+
+        private void BTNsubtracao_Click(object sender, EventArgs e)
+        {
+            if (valida_entrada())
+            {
+                // cálculo específico deste "objeto_evento"
+                result = (double)(numero1 - numero2);
+                saida(Color.Green);
+            }
+        }
+
+        private void BTNmultiplicacao_Click(object sender, EventArgs e)
+        {
+            if (valida_entrada())
+            {
+                // cálculo específico deste "objeto_evento"
+                result = (double)(numero1 * numero2);
+                saida(Color.Blue);
+            }
+        }
+
+        private void BTNdivisao_Click(object sender, EventArgs e)
+        {
+            if (valida_entrada())
+            {
+                // cálculo específico deste "objeto_evento"
+                if (numero2 == 0)
+                {
+                    LBLresultado.Text = "erro:div_0";
+                    LBLresultado.BackColor = Color.Salmon;
+                }
+                else
+                {
+                    result = (double)(numero1 / numero2);
+                    saida(Color.Red);
+                }
+            }
+        }
+
+        private void BTNlimpar_Click(object sender, EventArgs e)
+        {
+            // Iniciamos com zero, ao invés de  TXTnumero1.Clear();
+            TXTnumero1.Text = "0";
+            TXTnumero2.Text = "0";
+            LBLsaida.Text = "";
+            LBLsaida.BackColor = Color.Beige;
+        }
+
+        private void BTNsair_Click(object sender, EventArgs e)
+        {
+            // Fechar o programa
+            this.Close();
+        }
+    }
+}
---
+++ Calculator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB08
+{
+    public partial class Calculadora : Form
+    {
+        public float num1;
+        public float num2;
+
+        public Calculadora()
+        {
+            InitializeComponent();
+
+        }
+
+        private void entrada() // função não associada a objeto/evento.
+        {
+            num1 = float.Parse(numero1.Text);
+            num2 = float.Parse(numero2.Text);
+            // A estrutura deste procedimento é falha porque tem potencial
+            // de produzir erro de run-time, considerando que o float.Parse
+            // vai dar erro se o usuário deixar o campo vazio ou digitar
+            // qualquer caractere não numérico.
+            // A versão Lab03b vai corrigir isso e estruturar melhor!
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Chama o botão limpar.
+            BTNlimpar.PerformClick();
+        }
+
+        private void saida(Color cor)
+        {   // Recebe parâmetro COR que é usado no segundo comando abaixo.
+            // Este parâmetro é do tipo Color e seu valor é dado por Color.nome_da_cor
+            //LBLsaida.Text = result.Text;
+            result.BackColor = cor;
+        }
+
+        private void BTNSair_Click(object sender, EventArgs e)
+        {
+            // Fechar o programa
+            this.Close();
+        }
+
+        private void BTNLimpar_Click(object sender, EventArgs e)
+        {
+            // Iniciamos com zero, ao invés de TXTnumero1.Clear();
+            numero1.Text = "0";
+            numero2.Text = "0";
+            LBLsaida.BackColor = Color.Beige;
+        }
+
+        private void BTNDiv_Click(object sender, EventArgs e)
+        {
+            entrada();
+            // cálculo específico deste "objeto_evento"
+            if (num2 == 0)
+            {
+                result.Text = "Erro";
+                result.BackColor = Color.Salmon;
+            }
+            else
+            {
+                result.Text = (num1 / num2).ToString();
+                saida(Color.Red);
+            }
+        }
+
+        private void BTNMult_Click(object sender, EventArgs e)
+        {
+            entrada();
+            // cálculo específico deste "objeto_evento"
+            result.Text = (num1 * num2).ToString();
+            saida(Color.Blue);
+        }
+
+        private void BTNSub_Click(object sender, EventArgs e)
+        {
+            entrada();
+            // cálculo específico deste "objeto_evento"
+            result.Text = (num1 - num2).ToString();
+            saida(Color.Green);
+        }
+
+        private void BTNSomar_Click(object sender, EventArgs e)
+        {
+            entrada();
+            // cálculo específico deste "objeto_evento"
+            result.Text = (num1 + num2).ToString();
+            saida(Color.Silver);
+        }
+
+        private void TXTnumero1_TextChanged(object sender, EventArgs e)
+        {
+            // Programar o evento "TextChanged" é uma alternativa interessante.
+            // Entretanto, deve-se cuidar para que o processo de "Limpar" as
+            // caixas de entrada não dê erro (caso mova um valor vazio), porque
+            // a conversão de "" (vazio) para um tipo numérico é inválida.
+            //num1 = float.Parse(TXTnumero1.Text);
+            //num2 = float.Parse(TXTnumero2.Text);
+            // Os comandos acima, se programados aqui e no mesmo evento do  TXTvalor2, 
+            // substituem a função entrada(), com o cuidado de mover "0" ao
+            // invés de "" para o .Text no procedimento BTNlimpar_Click .
+        }
+
+        private void LBLsaida_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
